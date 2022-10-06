2174ecfee2f7ed19f42a7b09a8b9b8921e326e5a
Ivan-Samoylenko <ivansamoylenko@ukr.net>
2022-10-06 11:42:16 +0300

    перша частина бази

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 // підключення основного файлу стилів
 import "./styles/main.scss";
+
+import desktopRules from "./js/game-rules/desktop-game-rules.json";
+
+const myDiv = document.createElement("div");
+myDiv.classList.add("modal");
+
+function makeRulesInfo() {
+  return `<p>${desktopRules["paragraph-one"]}</p>
+  <p>${desktopRules["divider-one"]}</p>
+  <p>${desktopRules["paragraph-two"]}</p>
+  <p>${desktopRules["paragraph-three"]}</p>
+  <p>${desktopRules["divider-two"]}</p>
+  <p>${desktopRules["paragraph-four"]}</p>`;
+}
+
+myDiv.innerHTML = makeRulesInfo();
